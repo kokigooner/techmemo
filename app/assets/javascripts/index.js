cb2f2a423127f39e1740af2b257e15abf742d991
赤嶺昂樹 <akaminekoki@akaminenoborujunoMacBook-Air.local>
2019-08-30 16:54:29 +0900

    deploy-and-jquery

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,7 @@
+$(function(){
+  $(' p.word-box__bottom__show').click(function(e){
+    e.preventDefault();
+    $(this).next('ul.word-box__bottom__mean').slideToggle();
+    // $('.word-box__bottom__mean').toggle();
+  })
+})
